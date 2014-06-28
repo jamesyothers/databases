020cb25e3be9dc00281712b12d6a4ee1aba4dd95
Hack Reactor Students <james.yothers@gmail.com>
2014-06-28 10:44:33 -0700

    Pass tests

--- SQL/spec/LiveSpec.js
+++ SQL/spec/LiveSpec.js
@@ -32,26 +32,26 @@ describe("Persistent Node Chat Server", function() {
   it("Should insert posted messages to the DB", function(done) {
     // Post a message to the node chat server:
     request({method: "POST",
-             uri: "http://127.0.0.1:8080/classes/room1",
-             form: {username: "Valjean",
-                    message: "In mercy's name, three days is all I need."}
+             uri: "http://localhost:3000/classes/room1",
+             json: {username: "Valjean",
+                    text: "In mercy's name, three days is all I need."}
             },
             function(error, response, body) {
               /* Now if we look in the database, we should find the
                * posted message there. */
-
-              var queryString = "";
+              console.log('err: ', error);
+              var queryString = "SELECT * FROM messages";
               var queryArgs = [];
               /* TODO: Change the above queryString & queryArgs to match your schema design
                * The exact query string and query args to use
                * here depend on the schema you design, so I'll leave
                * them up to you. */
-              dbConnection.query( queryString, queryArgs,
+              dbConnection.query(queryString, queryArgs,
                 function(err, results, fields) {
                   // Should have one result:
                   expect(results.length).to.equal(1);
-                  expect(results[0].username).to.equal("Valjean");
-                  expect(results[0].message).to.equal("In mercy's name, three days is all I need.");
+                  expect(results[0].author).to.equal(1);
+                  expect(results[0].text).to.equal("In mercy's name, three days is all I need.");
                   /* TODO: You will need to change these tests if the
                    * column names in your schema are different from
                    * mine! */
@@ -63,7 +63,7 @@ describe("Persistent Node Chat Server", function() {
 
   it("Should output all messages from the DB", function(done) {
     // Let's insert a message into the db
-    var queryString = "";
+    var queryString = "INSERT into messages (createdAt, text, author, messageRoom) VALUES (NOW(), " + dbConnection.escape("Javert 'Men like you can never change!'") + ", 1, 2)";
     var queryArgs = ["Javert", "Men like you can never change!"];
     /* TODO - The exact query string and query args to use
      * here depend on the schema you design, so I'll leave
@@ -71,13 +71,15 @@ describe("Persistent Node Chat Server", function() {
 
     dbConnection.query( queryString, queryArgs,
       function(err, results, fields) {
+        console.log(err);
         /* Now query the Node chat server and see if it returns
          * the message we just inserted: */
-        request("http://127.0.0.1:8080/classes/room1",
+        request("http://localhost:3000/classes/4chan",
           function(error, response, body) {
-            var messageLog = JSON.parse(body);
-            expect(messageLog[0].username).to.equal("Javert");
-            expect(messageLog[0].message).to.equal("Men like you can never change!");
+            var messageLog = JSON.parse(body).results;
+            console.log(messageLog);
+            expect(messageLog[0].username).to.equal('Valjean');
+            expect(messageLog[0].text).to.equal("Javert 'Men like you can never change!'");
             done();
           });
       });
