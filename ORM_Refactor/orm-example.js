b9be4c4d0949c05e1e3ff3a51849be9809465274
Hack Reactor Students <james.yothers@gmail.com>
2014-06-28 12:37:06 -0700

    Sequelize is creating tables

--- ORM_Refactor/orm-example.js
+++ ORM_Refactor/orm-example.js
@@ -4,7 +4,7 @@
  */
 
 var Sequelize = require("sequelize");
-var sequelize = new Sequelize("chatter", "root", "");
+var sequelize = new Sequelize("chatterbox", "root", "");
 /* TODO this constructor takes the database name, username, then password.
  * Modify the arguments if you need to */
 
@@ -14,29 +14,37 @@ var User = sequelize.define('User', {
   username: Sequelize.STRING
 });
 
-var Message = sequelize.define('Message' {
-  userid: Sequelize.INTEGER,
-  text: Sequelize.STRING,
-  roomname: Sequelize.STRING
+var Message = sequelize.define('Message', {
+  text: Sequelize.STRING
 });
+
+var Room = sequelize.define('Room', {
+  roomName: Sequelize.STRING
+});
+
+// setup relationships between tables
+// http://runnable.com/UXgrne_v2oZyAACN/how-to-use-associations-in-sequelize-for-node-js
+Room.hasMany(Message);
+User.hasMany(Message);
 
 /* .sync() makes Sequelize create the database table for us if it doesn't
  *  exist already: */
-User.sync().success(function() {
-  /* This callback function is called once sync succeeds. */
+// use sequelize.sync() to sync across all tables
+sequelize.sync().success(function() {
+  // /* This callback function is called once sync succeeds. */
 
-  // now instantiate an object and save it:
-  var newUser = User.build({username: "Jean Valjean"});
-  newUser.save().success(function() {
+  // // now instantiate an object and save it:
+  // var newUser = User.build({username: "Jean Valjean"});
+  // newUser.save().success(function() {
 
-    /* This callback function is called once saving succeeds. */
+  //   /* This callback function is called once saving succeeds. */
 
-    // Retrieve objects from the database:
-    User.findAll({ where: {username: "Jean Valjean"} }).success(function(usrs) {
-      // This function is called back with an array of matches.
-      for (var i = 0; i < usrs.length; i++) {
-        console.log(usrs[i].username + " exists");
-      }
-    });
-  });
+  //   // Retrieve objects from the database:
+  //   User.findAll({ where: {username: "Jean Valjean"} }).success(function(usrs) {
+  //     // This function is called back with an array of matches.
+  //     for (var i = 0; i < usrs.length; i++) {
+  //       console.log(usrs[i].username + " exists");
+  //     }
+  //   });
+  // });
 });
